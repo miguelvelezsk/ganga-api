e728884824bb8b88955072b2e17d1b8ebe71c826
miguelvelezsk <miguelvelezaguirre10@gmail.com>
2026-04-22 14:09:13 -0500

    feat: configure CORS middleware to allow requests from local development frontend

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from orchestrator import orchestrator
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 @app.get("/search")
 async def search_product(product_name: str):
